a4662893f91a9e3931f67d0ed26e6e462a26638b
Saad Khaleeq <saadkhaleeq610@gmail.com>
2024-05-21 04:03:48 -0700

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,9 +6,11 @@ export default function Header() {
             <a href="/"><h1 className='font-medium'>Free<span className='text-blue-400 bold'>Script</span></h1></a>
             <div className='gap-4 flex items-center '>
                 <a href="/" className='flex items-center gap-2 specialBtn px-3 py-2 rounded-lg text-blue-400'>
-                    <p>New</p>
+                    <p className='font-semibold'>New</p>
                     <i className="fa-solid fa-plus"></i>
                 </a>
+                <div><button className='flex items-center gap-2 specialBtn px-3 py-2 rounded-lg text-blue-400 font-semibold'>My LinkedIn</button>
+                </div>
             </div>
         </header>
     )
